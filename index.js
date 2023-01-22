65b9ec5a37d32790894451f2e0fa699b14a1db0b
Leon Barth <116115776+BarthMedia@users.noreply.github.com>
2023-01-22 02:39:44 +0000

    prettier installed?

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 /* [BMG.studio] Test scripts to figure out the best infrastructure to build studio form */
+'use strict'
 
 // Import
 const hello = require('./hello')
 
-
 console.log( hello )
-
 
 "updated version. Lol"
