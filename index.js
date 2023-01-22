27cc15c2c9c06ac0e517c7c61219160c0bba9a57
Leon Barth <116115776+BarthMedia@users.noreply.github.com>
2023-01-22 01:42:57 +0000

    interesting

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-console.log("hi")
+/* [BMG.studio] Test scripts to figure out the best infrastructure to build studio form */
+
+// Import
+const hello = require('./hello')
+
+
+console.log( hello )
+
 
 "updated version. Lol"
